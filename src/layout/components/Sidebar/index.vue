1b95430deef2bf5db55aea5cc64499c416040c5a
cheng <64277988+chengQVQ@users.noreply.github.com>
2025-06-19 17:15:08 +0800

    父菜单权限

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,51 +1,53 @@
 <template>
   <div class="has-logo">
-    <logo  :collapse="isCollapse" />
+    <logo :collapse="isCollapse" />
     <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu
-        :default-active="activeMenu"
-        :collapse="isCollapse"
-        :background-color="variables.menuBg"
-        :text-color="variables.menuText"
-        :unique-opened="false"
-        :active-text-color="variables.menuActiveText"
-        :collapse-transition="false"
-        mode="vertical"
-      >
-        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+      <el-menu :default-active="activeMenu" :collapse="isCollapse" :background-color="variables.menuBg"
+        :text-color="variables.menuText" :unique-opened="false" :active-text-color="variables.menuActiveText"
+        :collapse-transition="false" mode="vertical">
+        <sidebar-item v-for="route in authRoutes" :key="route.path" :item="route" :base-path="route.path" />
       </el-menu>
     </el-scrollbar>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Logo from './Logo'
-import SidebarItem from './SidebarItem'
-import variables from '@/styles/variables.scss'
+import { mapGetters } from "vuex";
+import Logo from "./Logo";
+import SidebarItem from "./SidebarItem";
+import variables from "@/styles/variables.scss";
 
 export default {
   components: { SidebarItem, Logo },
+  data() {
+    return {
+      authRoutes: [],
+    };
+  },
   computed: {
-    ...mapGetters([
-      'sidebar',
-      'routes'
-    ]),
-    activeMenu () {
-      const route = this.$route
-      const { meta, path } = route
+    ...mapGetters(["sidebar", "routes", "userName"]),
+    activeMenu() {
+      const route = this.$route;
+      const { meta, path } = route;
       // if set path, the sidebar will highlight the path you set
       if (meta.activeMenu) {
-        return meta.activeMenu
+        return meta.activeMenu;
       }
-      return path
+      return path;
     },
-    variables () {
-      return variables
+    variables() {
+      return variables;
     },
-    isCollapse () {
-      return !this.sidebar.opened
+    isCollapse() {
+      return !this.sidebar.opened;
+    },
+  },
+  created() {
+    if (this.userName === "admin") {
+      this.authRoutes = this.routes;
+    } else {
+      this.authRoutes = this.routes.filter((i) => !i.isAuth)
     }
-  }
-}
+  },
+};
 </script>
